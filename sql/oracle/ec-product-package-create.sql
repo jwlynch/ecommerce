d0702985ce0d950ff094f919402b234076da9a1a
cvs2svn <oct@openacs.org>
2003-11-03 08:30:39 +0000

    This commit was manufactured by cvs2svn to create tag 'pre-ora9i-port'.
    Sprout from oacs-5-0 2003-11-03 08:30:31 UTC cvs2svn <oct@openacs.org> 'This commit was manufactured by cvs2svn to create branch 'oacs-5-0'.'
    Cherrypick from master 2003-08-28 09:41:53 UTC Lars Pind  <lars@pinds.com> 'Merged 4.6.4d1 (unreleased) to HEAD. This marks the end of the life of the oacs-4-6 branch.':
        www/admin/products/extras-upload-2.tcl
        tcl/ecommerce-procs.tcl
        www/admin/products/delete-2-oracle.xql
        www/admin/products/delete-2.tcl
        www/admin/customer-service/spam-2.xql
        www/admin/products/list.tcl
        sql/oracle/ec-product-package-create.sql

--- sql/oracle/ec-product-package-create.sql
+++ sql/oracle/ec-product-package-create.sql
@@ -45,7 +45,7 @@ as
   announcements_expire  in ec_products.announcements_expire%TYPE	default null
  ) return acs_objects.object_id%TYPE;
 
- procedure del (
+ procedure delete (
   product_id in ec_products.product_id%TYPE
  );
 
@@ -105,15 +105,15 @@ as
      return v_object_id;
  end new;
 
- procedure del (
+ procedure delete (
   product_id in ec_products.product_id%TYPE
  )
  is
  begin
      delete from ec_products
-         where product_id = ec_product.del.product_id;
-     acs_object.del(product_id);
- end del;
+         where product_id = ec_product.delete.product_id;
+     acs_object.delete(product_id);
+ end delete;
 
 end ec_product;
 /
